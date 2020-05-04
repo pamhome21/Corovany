c73f5d734b562f67d384e7678994e5dd99d0b475
Pam <pamhome21@gmail.com>
2020-05-04 20:21:23 +0500

    More basic concept of concept

--- Corovany/logic/CombatCore.cs
+++ Corovany/logic/CombatCore.cs
@@ -22,6 +22,8 @@ namespace Corovany.logic
             public int Initiative { get; set; }
             public UnitState State { get; }
             public Dictionary<string, Action<ICombatUnit>> Perks { get; }
+            public void ApplyHpDamage(int dmg);
+            public void ApplyMpDamage(int dmg);
         }
 
         public class PlayerCombatUnit: ICombatUnit
@@ -33,8 +35,22 @@ namespace Corovany.logic
             public int MaxSpecialPoints { get; set; }
             public int SpecialPoints { get; private set; }
             public int Initiative { get; set; }
-            public UnitState State { get; }
+            public UnitState State { get; private set; }
             public Dictionary<string, Action<ICombatUnit>> Perks { get; private set; }
+            
+            public void ApplyHpDamage(int dmg)
+            {
+                HealthPoints-= dmg;
+                if (HealthPoints <= 0)
+                    State = UnitState.Dead;
+            }
+
+            public void ApplyMpDamage(int dmg)
+            {
+                MoralePoints-= dmg;
+                if (MoralePoints <= 0 && State != UnitState.Dead)
+                    State = UnitState.Escaped;
+            }
 
             public PlayerCombatUnit(Characters.PlayerChar character)
             {
@@ -47,6 +63,11 @@ namespace Corovany.logic
                 Initiative = character.Initiative;
                 State = UnitState.Fine;
                 Perks = character.CharClass.Perks;
+            }
+
+            public void CastPerk(string perkName, ICombatUnit target)
+            {
+                Perks[perkName](target);
             }
         }
     }
