4fed13a67a24933dda6ab095712d2975898d36c5
Pam <pamhome21@gmail.com>
2020-05-04 20:01:21 +0500

    Basic concept of concept

---
+++ Corovany/logic/Characters.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Corovany.logic
+{
+    public class Characters
+    {
+        private const double LvlFactor = 0.66;
+        public class CharacterClass
+        {
+            public string Name { get; }
+            public int HealthPoints { get; }
+            public int MoralePoints { get; }
+            public int SpecialPoints { get; }
+            public int Initiative { get; }
+            public Dictionary<string, Action<CombatCore.ICombatUnit>> Perks { get; }
+
+            public CharacterClass(string name, int hp, int mp, int sp, int initiative,
+                Dictionary<string, Action<CombatCore.ICombatUnit>> perks)
+            {
+                Name = name;
+                HealthPoints = hp;
+                MoralePoints = mp;
+                SpecialPoints = sp;
+                Initiative = initiative;
+                Perks = perks;
+            }
+        }
+        public class PlayerChar
+        {
+            public string Name { get; private set; }
+            public CharacterClass CharClass { get; set; } 
+            public int Level { get; set; }
+            public int Initiative { get; set; }
+            public int HealthPoints { get; set; }
+            public int MoralePoints { get; set; }
+            public int SpecialPoints { get; set; }
+
+            public PlayerChar(string name, CharacterClass charClass, int level)
+            {
+                Name = name;
+                CharClass = charClass;
+                Level = level;
+                UpdateLvl();
+            }
+
+            public void Rename(string newName)
+            {
+                if (newName.Length>0)
+                    Name = newName;
+            }
+
+            public void UpdateLvl()
+            {
+                var isNotFirstLvl = Level > 1;
+                Initiative = isNotFirstLvl ? (int)(CharClass.Initiative * Level * LvlFactor) : CharClass.Initiative;
+                HealthPoints = isNotFirstLvl ? (int)(CharClass.HealthPoints * Level * LvlFactor) : CharClass.HealthPoints;
+                MoralePoints = isNotFirstLvl ? (int)(CharClass.MoralePoints * Level * LvlFactor) : CharClass.MoralePoints;
+                SpecialPoints = isNotFirstLvl ? (int)(CharClass.SpecialPoints * Level * LvlFactor) : CharClass.SpecialPoints;
+            }
+        }
+    }
+}
---
+++ Corovany/logic/CombatCore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Corovany.logic
+{
+    public class CombatCore
+    {
+        public enum UnitState
+        {
+            Fine = 0,
+            Escaped = 1,
+            Dead = 2
+        }
+        public interface ICombatUnit
+        {
+            public int MaxHealthPoints { get; set; }
+            public int HealthPoints { get; }
+            public int MaxMoralePoints { get; set; }
+            public int MoralePoints { get; }
+            public int MaxSpecialPoints { get; set; }
+            public int SpecialPoints { get; }
+            public int Initiative { get; set; }
+            public UnitState State { get; }
+            public Dictionary<string, Action<ICombatUnit>> Perks { get; }
+        }
+
+        public class PlayerCombatUnit: ICombatUnit
+        {
+            public int MaxHealthPoints { get; set; }
+            public int HealthPoints { get; private set; }
+            public int MaxMoralePoints { get; set; }
+            public int MoralePoints { get; private set; }
+            public int MaxSpecialPoints { get; set; }
+            public int SpecialPoints { get; private set; }
+            public int Initiative { get; set; }
+            public UnitState State { get; }
+            public Dictionary<string, Action<ICombatUnit>> Perks { get; private set; }
+
+            public PlayerCombatUnit(Characters.PlayerChar character)
+            {
+                MaxHealthPoints = character.HealthPoints;
+                HealthPoints = MaxHealthPoints;
+                MaxMoralePoints = character.MoralePoints;
+                MoralePoints = MaxMoralePoints;
+                MaxSpecialPoints = character.SpecialPoints;
+                SpecialPoints = MaxSpecialPoints;
+                Initiative = character.Initiative;
+                State = UnitState.Fine;
+                Perks = character.CharClass.Perks;
+            }
+        }
+    }
+}
