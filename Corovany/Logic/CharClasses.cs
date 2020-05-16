89d1d32f3efc5f20bebb4507c31da6b611924ceb
Pam <pamhome21@gmail.com>
2020-05-17 00:42:17 +0500

    Added new perks

--- Corovany/Logic/CharClasses.cs
+++ Corovany/Logic/CharClasses.cs
@@ -14,12 +14,19 @@ namespace Corovany.Logic
         {
             private static readonly List<CharacterCore.Perk> Perks = new List<CharacterCore.Perk>()
             {
-                new CharacterCore.Perk("Power Blade", "human_skill_power_blade.png",
-                    $"Powerful strike that deals 50HP damage",
+                new CharacterCore.Perk("Power Blade", "human_skill_power_blade",
+                    $"Powerful strike that deals 50HP dmg",
                     10, 0, 1, unit =>
                 {
                     unit.ApplyHpDamage(50);
-                })
+                }),
+                new CharacterCore.Perk("Fire Blade", "human_skill_fire_blade",
+                    $"Fiery attack that deals 30HP dmg and 20MP dmg",
+                    10, 0, 1, unit =>
+                    {
+                        unit.ApplyHpDamage(30);
+                        unit.ApplyMpDamage(20);
+                    })
             };
             public static readonly CharacterCore.CharacterClass Class = new CharacterCore.CharacterClass("Human", 
                 100, 100, 100, 4, Perks);
